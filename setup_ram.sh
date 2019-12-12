160b25401e039ab9142b4b18fa5a41fd39c0236b
ldessemon2 <46007787+ldessemon2@users.noreply.github.com>
2019-12-12 00:41:22 -0500

    v1.1

--- setup_ram.sh
+++ setup_ram.sh
@@ -1,3 +1,11 @@
 #!/bin/sh
-read -p 'Enter your Personal access (GitHub) token : ' GIT_TOKEN
+current_time=$(date "+%Y.%m.%d-%H.%M.%S")
+ram_log_name=ram
+RAM_SETUP_LOG=$ram_log_name.$current_time.log
+MONGODB_PORT=27017
+read -sp 'Enter your Personal access (GitHub) token : ' GIT_TOKEN
+
 git clone https://${GIT_TOKEN}@github.com/Renaps/ram.git
+git clone https://${GIT_TOKEN}@github.com/Renaps/ram_core.git
+
+./ram/setup/install.sh
